60c371fddf4d7d32ab473c3664c9cf85465bcbbd
Shivam Joshi <101315624+Shivam3090@users.noreply.github.com>
2024-07-14 15:57:59 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
 @echo off
 
+REM Activate your Anaconda environment
+call "C:\Users\31220\anaconda3\Scripts\activate.bat" new_env
+
 REM Install Rust if not already installed
 where rustc >nul 2>nul
 IF %ERRORLEVEL% NEQ 0 (
