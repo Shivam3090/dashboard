4997e947147eedcfef9d430d8894a9d759c477e6
Shivam Joshi <101315624+Shivam3090@users.noreply.github.com>
2024-07-14 16:07:06 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,15 +3,8 @@
 REM Activate your Anaconda environment
 call "C:\Users\31220\anaconda3\Scripts\activate.bat" new_env
 
-REM Install Rust if not already installed
-where rustc >nul 2>nul
-IF %ERRORLEVEL% NEQ 0 (
-    curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-    call "%USERPROFILE%\.cargo\env.bat"
-)
-
-REM Ensure Cargo environment is sourced
-call "%USERPROFILE%\.cargo\env.bat"
+REM Set PATH to Rust and Cargo
+set PATH=%PATH%;C:\Users\31220\.cargo\bin
 
 REM Install Python dependencies
 pip install -r requirements.txt
